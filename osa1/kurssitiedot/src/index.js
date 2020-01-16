fd6ce6421fc0409131de5450e4c2b101f140e5e0
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-01-16 22:24:22 +0200

    Fix first exercise, second exercise

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -6,17 +6,17 @@ const Header = (props) => (
 )
 
 const Content = (props) => (
-  <>
+  <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
+  </div>
+)
+
+const Part = (props) => (
   <p>
-    {props.part1} {props.exercises1}
+    {props.part} {props.exercises}
   </p>
-  <p>
-    {props.part2} {props.exercises2}
-  </p>
-  <p>
-    {props.part3} {props.exercises3}
-  </p>
-  </>
 )
 
 const Total = (props) => (
@@ -38,7 +38,6 @@ const App = () => {
       <Content part1={part1} part2={part2} part3={part3}
         exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
