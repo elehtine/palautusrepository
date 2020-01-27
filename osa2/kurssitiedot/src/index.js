28114b089a78b6426f4ba695b095b702083194b7
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-01-27 15:05:58 +0200

    Exercise 2 from part 2

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -5,12 +5,22 @@ const Course = ({ course }) => (
   <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 )
 
 const Header = ({ name }) => (
   <h1>{name}</h1>
 )
+
+const Total = ({ parts }) => {
+  const total = parts.reduce( (s, p) => (s + p.exercises), 0)
+  return (
+    <div>
+      <b>total of {total} exercises</b>
+    </div>
+  )
+}
 
 const Content = ({ parts }) => {
   return (
