b155cd0ea3e0d622eb58856d9b0b898cc32e5019
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-01-30 22:19:36 +0200

    Exercise 12.4, some problems with weatherstack

--- osa2/maiden-tiedot/src/index.js
+++ osa2/maiden-tiedot/src/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const Find = ({ handleSearch, search }) => {
   return (
     <div>
@@ -30,7 +32,7 @@ const CountryList = ({ countries, setSearch }) => {
   )
 }
 
-const Country = ({ country }) => {
+const Country = ({ country, weather }) => {
   return (
     <div>
       <h1>{country.name}</h1>
@@ -43,16 +45,45 @@ const Country = ({ country }) => {
         )}
       </ul>
       <img src={country.flag} width="160" alt="NO FLAG"/>
+      <Weather weather={weather} />
+    </div>
+  )
+}
+
+const Weather = ({ weather }) => {
+  if (weather === null) {
+    return (
+      <div>
+        null
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Weather in {weather.location.name}</h1>
+      <p>
+        <b>temperature:</b> {weather.current.temperature}
+      </p>
+      <img src={weather.current.weather_icons[0]} width="160" />
+      <p>
+        <b>wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}
+      </p>
     </div>
   )
 }
 
 const App = () => {
   const [ countries, setCountries ] = useState([])
+  const [ city, setCity ] = useState('')
   const [ search, setSearch ] = useState('')
+  const [ weather, setWeather ] = useState(null)
 
   const countriesToShow = countries.filter(country =>
     country.name.toLowerCase().includes(search.toLowerCase()))
+
+  if (countriesToShow.length === 1 && city === '') {
+    setCity(countriesToShow[0].capital)
+  }
 
   useEffect(() => {
     axios
@@ -62,8 +93,22 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    if (city !== '') {
+      setWeather(null)
+      axios
+        .get('http://api.weatherstack.com/current' +
+          '?access_key=' + api_key +
+          '&query=' + city)
+        .then(response => {
+          setWeather(response.data)
+        })
+    }
+  }, [ city ])
+
   const handleSearch = (event) => {
     setSearch(event.target.value)
+    setCity('')
   }
 
   if (countriesToShow.length === 1) {
@@ -75,6 +120,7 @@ const App = () => {
         />
         <Country
           country={countriesToShow[0]}
+          weather={weather}
         />
       </div>
     )
