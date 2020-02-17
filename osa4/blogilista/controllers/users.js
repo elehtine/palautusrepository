0b7ea08f30026148615fe0c940c6b75ab2b54566
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-02-17 21:27:33 +0200

    Add populate, add token

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -2,6 +2,14 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (req, res) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { url: 1, title: 1, author: 1 })
+
+  res.json(users.map(user => user.toJSON()))
+})
+
 usersRouter.post('/', async (req, res) => {
   const body = req.body
 
