ea585bb8c11c5f77074134955c91a277b1460e67
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-02-17 22:46:15 +0200

    Fix middleware

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -4,6 +4,8 @@ const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     req.token = authorization.substring(7)
+  } else {
+    req.token = null
   }
   next()
 }
