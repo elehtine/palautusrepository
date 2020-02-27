30cc7105d3f87533bd1a9c49e0b6f08ca11cdff0
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-02-27 21:39:27 +0200

    Refactor store to own file, add notification reducer

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
+  const notification = useSelector(state => state.notification)
   return (
     <div>
       <h2>Anecdotes</h2>
+      <div>{notification}</div>
       <AnecdoteForm />
       <AnecdoteList />
     </div>
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -41,7 +41,7 @@ const compareAnecdotes = (a, b) => {
   return b.votes - a.votes
 }
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'VOTE':
       const id = action.data.id
@@ -63,4 +63,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
