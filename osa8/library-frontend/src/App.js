018db7022c32c02dd086827ef7d5938129d9d3bb
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-03-15 16:38:33 +0200

    Add login

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('user-token')
+    if (token) {
+      setToken(token)
+    }
+  }, [])
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+  }
 
   return (
     <div>
+      <div>{token}</div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button
+          onClick={() => setPage('login')}
+          style={ token === null ? {} : { display: 'none' } }
+        >login</button>
+        <button
+          onClick={() => setPage('add')}
+          style={ token === null ? { display: 'none' } : {} }
+        >add book</button>
+        <button
+          onClick={logout}
+          style={ token === null ? { display: 'none' } : {} }
+        >logout</button>
       </div>
 
       <Authors
@@ -24,6 +50,12 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <Login
+        show={page === 'login'}
+        setToken={setToken}
+        setPage={setPage}
       />
 
     </div>
